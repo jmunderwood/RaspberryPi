080f33a883bd6f8ef12a31c1fa55bf6117fe32e1
Jason Underwood <jmunderwood@gmail.com>
2018-01-14 16:13:22 -0500

    Added variable time and comments

--- blink.py
+++ blink.py
@@ -1,12 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
+#setup Pi Mode
 GPIO.setmode(GPIO.BCM)
 
+#set pin 4 as output pin
 GPIO.setup(4, GPIO.OUT)
 
+blinktime = .5
 while True:
 	GPIO.output(4, GPIO.HIGH)
-	time.sleep(1)
+	time.sleep(blinktime)
 	GPIO.output(4, GPIO.LOW)
-	time.sleep(1)
+	time.sleep(blinktime)
